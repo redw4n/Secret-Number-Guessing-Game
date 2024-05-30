d13567480dc9358744be5c6fdbed935a4a2d7725
Redwanul karim <78757015+redw4n@users.noreply.github.com>
2024-05-30 13:36:27 +0600

    Add files via upload

---
+++ Guessing Number Program cpp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+   int secretNum = 11;
+   int guess;
+
+
+   while(secretNum != guess) {
+        cout << "Enter The Guess Number: ";
+        cin >> guess;
+
+
+   }
+
+
+   cout << "You Win!";
+
+
+        return 0;
+}
